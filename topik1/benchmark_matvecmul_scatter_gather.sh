7cce54469677c83f6a821da248dead9b82dc2be3
aselhid <asel.hidayat@gmail.com>
2020-03-10 22:23:47 +0700

    added mvmsg script

---
+++ topik1/benchmark_matvecmul_scatter_gather.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+procs=(2 4 8 16 32)
+
+mpicc -o mvmsg matrix_vector_multiplication_scatter_gather.c
+chmod +x mvmsg
+
+echo "np,N,M,valid,job_time,fanout_time,fanin_time,"
+for i in "${procs[@]}"
+do
+    for j in {1..5}
+    do
+        mpirun --hostfile $(pwd)/../hostfile -np "$(($i + 1))" $(pwd)/mvmsg
+    done
+done
+echo ""
