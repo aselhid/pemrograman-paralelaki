d7973d4e8f4cc92b627e6ca152357e89ef1e47dc
aselhid <asel.hidayat@gmail.com>
2020-03-10 20:04:54 +0700

    fix benchmark script topik 1

--- topik1/benchmark_matmul.sh
+++ topik1/benchmark_matmul.sh
@@ -2,13 +2,15 @@
 
 procs=(2 4 8 16 32)
 
+mpicc -o mm matrix_multiplication.c
+chmod +x mm
+
 echo "np,N,valid,job_time,fanout_time,fanin_time,"
 for i in "${procs[@]}"
 do
     for j in {1..5}
     do
-        mpicc -o mm matrix_multiplication.c
-        mpirun --hostfile $(pwd)/../hostfile -np "$i" $(pwd)/ mm
+        mpirun --hostfile $(pwd)/../hostfile -np "$i" $(pwd)/mm
     done
 done
 echo ""
--- topik1/benchmark_matmul_scatter_gather.sh
+++ topik1/benchmark_matmul_scatter_gather.sh
@@ -2,13 +2,15 @@
 
 procs=(2 4 8 16 32)
 
+mpicc -o mmsg matrix_multiplication_scatter_gather.c
+chmod +x mmsg
+
 echo "np,N,valid,job_time,fanout_time,fanin_time,"
 for i in "${procs[@]}"
 do
     for j in {1..5}
     do
-        mpicc -o mmsg matrix_multiplication_scatter_gather.c
-        mpirun --hostfile $(pwd)/../hostfile -np "$i" $(pwd)/ mmsg 
+        mpirun --hostfile $(pwd)/../hostfile -np "$i" $(pwd)/mmsg 
     done
 done
 echo ""
